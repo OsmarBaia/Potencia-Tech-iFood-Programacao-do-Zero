b9c2679cd28ef356afee3a8f0bf4cb965a254178
Osmar Castro <osmar.neto23@gmail.com>
2023-10-17 00:27:33 -0300

    feat: Add UI and Dom manipulation

--- 3. Classes do Heroi/main.js
+++ 3. Classes do Heroi/main.js
@@ -1,50 +1,115 @@
+// Dom Elements
+const herosImgElement = document.querySelectorAll("#menu__image img");
+const actionButtonsElement = document.querySelectorAll("#menu__action button");
+const classNameElement = document.querySelector("#menu__className")
+const herosAttackContainer = document.querySelector("#container-heroAttack");
+
+let currentJob = 0;
+
+let playerHero;
+
+// Reset Sprites
+function Start(){
+    playerHero = new Hero();
+    herosImgElement.forEach( e => {
+        e.style.display = "none"
+    })    
+    herosImgElement[0].style.display = "";
+    // herosAttackContainer.style.display = "none";
+    
+}
+
+function toggleHeroAttackContainer(){
+    herosAttackContainer.style.display = herosAttackContainer.style.display !== "none" ? "none" : "";
+}
+
+function ChangeJob(button){
+    herosImgElement[currentJob].style.display = "none";    
+    if(button === actionButtonsElement[0]){
+        currentJob -= 1;
+    }else{
+        currentJob += 1;
+    }
+    
+    if( currentJob < 0){
+        currentJob = herosImgElement.length -1;
+    }
+    
+    if(currentJob > herosImgElement.length -1){
+        currentJob = 0;
+    }
+    
+    herosImgElement[currentJob].style.display = "";    
+    playerHero.job = playerHero.jobClass[currentJob];
+    classNameElement.innerText = playerHero.job.jobName;
+}
+
+function AppendText(job){
+    let classNameElement = document.querySelector("#container-heroAttack h2");
+    classNameElement.innerText = job.jobName;
+
+    // let textElement = document.createElement("p")
+    let textElement = document.querySelector("#container-heroAttack p");
+    textElement.innerHTML = `
+            O  <strong> ${job.jobName} </strong> atacou usando <strong> ${job.attackType}</strong>
+            `
+}
+
 class Job {
     constructor(jobName, attackType) {
         this.jobName = jobName;
-        this.attackType = attackType;
-    }
+        this.attackType = attackType;    
+    }   
+    
     Attack(){
-        console.log(`o ${this.jobName} atacou usando ${this.attackType}`)
+        AppendText(this);
     }
 }
 
 class Mage extends Job {
     constructor(){
-        super("Mago", "Magia");
+        super("mago", "magia");
     }    
 }
 
 class Warrior extends Job {
     constructor(){
-        super("Guerreiro", "Espada");
+        super("guerreiro", "espada");
     }
 }
 
 class Monk extends Job {
     constructor(){
-        super("Monge", "Artes Marciais");
+        super("monge", "artes marciais");
     }
 }
 
 class Ninja extends Job {
     constructor(){
-        super("Ninja", "Shuriken");
+        super("ninja", "shuriken");
     }
 }
 
-
-const character={
-    mage: new Mage(),
-    warrior: new Warrior(),
-    monk: new Monk(),
-    ninja: new Ninja()
+class Hero {            
+    jobClass = [
+        new Warrior(), 
+        new Mage(), 
+        new Monk(), 
+        new Ninja()
+    ];
+    
+    constructor() {
+        this.job = new Job();
+        this.job = this.jobClass[currentJob];
+    }
+    
+    Attack(){
+        this.job.Attack();
+    }
 }
 
-function main(){
-    character.mage.Attack();
-    character.warrior.Attack();
-    character.monk.Attack();
-    character.ninja.Attack();
+function ChooseHero(){
+    toggleHeroAttackContainer();
+    playerHero.Attack();
 }
 
-main();
